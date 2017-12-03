f67a44134d82a230828bbbdb5075bd5a9c409677
PedroHOMota <phmota@hotmail.com>
2017-12-03 20:31:03 +0000

    Added gameloop

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -6,13 +6,26 @@ public class DestroyByContact : MonoBehaviour {
 
     public GameObject explosion;
     public GameObject playerExplosion;
+    private GameController gameController;
+
+    int score;
+
+    void Start()
+    {
+        gameController = GameObject.FindWithTag("GameController").GetComponent <GameController>();
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary") return;
 
         Instantiate(explosion, transform.position, transform.rotation);
+        if (other.tag == "Player")
+        {
+            Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.EndGame();
+        }
+        gameController.UpdateScore(10);
 
-        //GetComponent<AudioSource>().Play();
         Destroy(other.gameObject);
         Destroy(gameObject);
     }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -8,28 +8,62 @@ public class GameController : MonoBehaviour
     public GameObject hazard;
     public int spawnCount;
     public float spawnWait;
+
+    public GUIText scoreText;
+    public GUIText restartText;
+    public GUIText gameOverText;
+
+    private bool gameOver, restart;
+    private int score=0;
+
     void Start ()
     {
-        StartCoroutine(SpawnWaves());	
-	}
-	
-	
+        StartCoroutine(SpawnWaves());
+        UpdateScore(0);
 
+        gameOver = restart = false;
+
+    }
+
+    public void UpdateScore(int newScore)
+    {
+        score += newScore;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void EndGame()
+    {
+        Debug.Log("Entrou");
+        gameOverText.text = "Game Over";
+        gameOver = true;
+    }
+
+    void Update()
+    {
+        if (restart == true)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+                Application.LoadLevel(Application.loadedLevel);
+        }
+    }
     IEnumerator SpawnWaves()
     {
         yield return new WaitForSeconds(1);
-        while (true) //Making waves
+        while (gameOver==false) //Making waves
         {
             for (int i = 0; i< spawnCount; i++)
             {
                 Vector3 spawnP = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0, spawnValues.z);
                 Quaternion spawnR = Quaternion.identity;
-                //Debug.Log(spawnR);
-                //Debug.Log(spawnP);
                 Instantiate(hazard, spawnP, spawnR);
                 yield return new WaitForSeconds(spawnWait);
             }
 
+            if (gameOver)
+            {
+                restart = true;
+                restartText.text = "Press P to restart";
+            }
             yield return new WaitForSeconds(2);
         }
     }
