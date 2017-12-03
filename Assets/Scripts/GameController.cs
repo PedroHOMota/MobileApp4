8e9ff56c566f79e27e565d2f53225f3bdf42cd4f
PedroHOMota <phmota@hotmail.com>
2017-12-03 17:16:12 +0000

    Added more gamelogic's scripts

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public Vector3 spawnValues;
+    public GameObject hazard;
+                                     
+    void Start ()
+    {
+        while(true)
+            SpawnWaves();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void SpawnWaves()
+    {
+        Vector3 spawnP = new Vector3(Random.Range(-spawnValues.x,spawnValues.x),0, spawnValues.z);
+        Quaternion spawnR = Quaternion.identity;
+        //Debug.Log(spawnR);
+        //Debug.Log(spawnP);
+        Instantiate(hazard, spawnP, spawnR);
+    }
+}
