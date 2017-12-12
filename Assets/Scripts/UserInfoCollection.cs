915fe3a258d5da0ea3986680d9669921298526e5
PedroHOMota <phmota@hotmail.com>
2017-12-12 18:33:34 +0000

    Removed debug button from game

--- Assets/Scripts/UserInfoCollection.cs
+++ Assets/Scripts/UserInfoCollection.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 [System.Serializable]
-public class UserInfoCollection
+public class UserInfoCollection //Unity doesn't parse json array,
 {
 	public LeaderboardUserInfo[] infos;
 }
